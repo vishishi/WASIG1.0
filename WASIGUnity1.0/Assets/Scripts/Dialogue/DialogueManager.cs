771573e60658400fa8334e7540f5f469cad479e2
lonelyharpsclub <125093087+lonelyharpsclub@users.noreply.github.com>
2025-07-28 22:14:28 +0100

    added some dialogue scripts

---
+++ WASIGUnity1.0/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] TextMeshPro dialogueText;
+    [SerializeField] Button nextButton;
+
+    //make an array of the snippet class
+    [SerializeField] DialogueSnippet[] dialogueSnippets;
+    int currentSnippetIndex = 0;
+
+    //trigger this on a button or an interactable
+    public void NextDialogueSnippet()
+    {
+        dialogueText.text = dialogueSnippets[currentSnippetIndex].dialogueSentenceEnglish;
+
+        //when FMOD play monologue sound by key here
+
+        //if we have reached the end of the array
+        if (currentSnippetIndex >= dialogueSnippets.Length)
+        {
+            Debug.Log("End of Dialogue");
+        }
+
+    }
+
+
+
+}
---
+++ WASIGUnity1.0/Assets/Scripts/Dialogue/DialogueSnippet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class DialogueSnippet
+{
+    [TextArea(3, 10)]
+    public string dialogueSentenceEnglish;
+
+    //when we put in FMOD add a string for the audio key
+    
+}
