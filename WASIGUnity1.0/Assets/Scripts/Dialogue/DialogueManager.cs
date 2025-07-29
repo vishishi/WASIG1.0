7e61180454d4ee5d81ff19add217af60c4650750
lonelyharpsclub <125093087+lonelyharpsclub@users.noreply.github.com>
2025-07-29 17:42:16 +0100

    working dialogue system script

--- WASIGUnity1.0/Assets/Scripts/Dialogue/DialogueManager.cs
+++ WASIGUnity1.0/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -3,20 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Meta.XR.MultiplayerBlocks.Fusion.Editor;
+using Unity.VisualScripting;
 
 public class DialogueManager : MonoBehaviour
 {
-    [SerializeField] TextMeshPro dialogueText;
+    [SerializeField] TextMeshProUGUI dialogueText;
     [SerializeField] Button nextButton;
-    [SerializeField] Button dialogueObject;
+    [SerializeField] GameObject dialogueObjectButton;
+    [SerializeField] GameObject dialogueScriptObject;
+    int numberOfDialogueObjects = 8;
 
     //make an array of the snippet class
     [SerializeField] DialogueSnippet[] dialogueSnippets;
     int currentSnippetIndex = 0;
 
-    //trigger this on a button or an interactable
-    public void NextDialogueSnippet()
+
+
+    private void Start()
     {
+        nextButton.onClick.RemoveAllListeners();
+        nextButton.onClick.AddListener(NextDialogueSnippet);
+    }
+
+    private void Update()
+    {
+
+
+        if (numberOfDialogueObjects > 0)
+        {
+            Debug.Log("there are no more dialogue objects");
+        }
+    }
+
+    public void StartConversation()
+    {
+        dialogueText.text = dialogueSnippets[currentSnippetIndex].dialogueSentenceEnglish;
+        dialogueObjectButton.SetActive(false);
+
+    }
+    
+
+    
+    //trigger this on a button or an interactable
+    void NextDialogueSnippet()
+    {
+        //increase index number of snippets
+        currentSnippetIndex++;
+
+        //update text every time button is pressed
         dialogueText.text = dialogueSnippets[currentSnippetIndex].dialogueSentenceEnglish;
 
         //when FMOD play monologue sound by key here
@@ -24,8 +59,16 @@ public class DialogueManager : MonoBehaviour
         //if we have reached the end of the array
         if (currentSnippetIndex >= dialogueSnippets.Length)
         {
-            Debug.Log("End of Dialogue");
+            EndThisDialogue();
         }
+
+    }
+
+    void EndThisDialogue()
+    {
+        Debug.Log("End of Dialogue");
+        numberOfDialogueObjects--;
+        dialogueScriptObject.SetActive(false);
 
     }
 
