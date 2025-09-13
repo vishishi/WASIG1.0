f28f4ad1f34d84065ab5f88ea2ab8aae2a551b61
lonelyharpsclub <125093087+lonelyharpsclub@users.noreply.github.com>
2025-09-13 21:58:19 +0100

    Added Hype Track Script

--- WASIGUnity1.0/Assets/Scripts/Audio/HyperTrackFadeInOut.cs
+++ WASIGUnity1.0/Assets/Scripts/Audio/HyperTrackFadeInOut.cs
@@ -19,25 +19,44 @@ public class HyperTrackFadeInOut : MonoBehaviour
     {
         if (score.isSuperCharged == true)
         {
-            FadeInHypeTrack();
+            StartCoroutine(FadeInHypeTrack());
         }
         else
         {
-            
+            StartCoroutine(FadeOutHypeTrack());
         }
     }
 
-    void FadeInHypeTrack()
+    IEnumerator FadeInHypeTrack()
     {
-        float timeToFade = 0.5f;
+        float timeToFade = 1f;
         float timeElapsed = 0;
 
         while (timeElapsed < timeToFade)
         {
             hypeTrackAudio.volume = Mathf.Lerp(0, 0.75f, timeElapsed / timeToFade);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+
         }
 
     }
+
+    IEnumerator FadeOutHypeTrack()
+    {
+        float timeToFade = 1f;
+        float timeElapsed = 0;
+
+        while (timeElapsed < timeToFade)
+        {
+            hypeTrackAudio.volume = Mathf.Lerp(0.75f, 0, timeElapsed / timeToFade);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+
+        }
+    }
+
+    
 
 
 
