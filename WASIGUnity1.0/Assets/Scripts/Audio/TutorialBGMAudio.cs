205e38ae26709311484f1ef06da532c8f0cd3f41
lonelyharpsclub <125093087+lonelyharpsclub@users.noreply.github.com>
2025-09-16 12:02:17 +0100

    Made in scene audio swapping script successfully

---
+++ WASIGUnity1.0/Assets/Scripts/Audio/TutorialBGMAudio.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBGMAudio : MonoBehaviour
+{
+
+    public AudioClip tutorialBGMtrack1, tutorialBGMtrack2, tutorialBGMtrack3;
+    public AudioSource tutorialBGMAudioSource;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tutorialBGMAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayTutorialBGMTrack(AudioClip currentTutorialBGMTrack)
+    {
+        
+        //tutorialBGMAudioSource.Stop();
+        tutorialBGMAudioSource.clip = currentTutorialBGMTrack;
+
+        if (currentTutorialBGMTrack == null)
+        {
+            Debug.LogError("AudioClip is NULL!");
+            return;
+        }
+
+        
+        tutorialBGMAudioSource.Play();
+        StartCoroutine(FadeInBGM());
+
+
+    }
+
+    public void StopTutorialBGMTrack()
+    {
+        StartCoroutine(FadeOutBGM());
+        
+
+    }
+
+    IEnumerator FadeInBGM()
+    {
+        float timeToFade = 1f;
+        float timeElapsed = 0;
+
+        while (timeElapsed < timeToFade)
+        {
+            tutorialBGMAudioSource.volume = Mathf.Lerp(0, 0.75f, timeElapsed / timeToFade);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+
+        }
+
+    }
+
+    IEnumerator FadeOutBGM()
+    {
+        float timeToFade = 2f;
+        float timeElapsed = 0;
+
+        while (timeElapsed < timeToFade)
+        {
+            tutorialBGMAudioSource.volume = Mathf.Lerp(0.75f, 0, timeElapsed / timeToFade);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+
+        }
+        tutorialBGMAudioSource.Stop();
+    }
+
+    // just for button test
+
+    public void PlayAudioOnButton()
+    {
+        PlayTutorialBGMTrack(tutorialBGMtrack1);
+    }
+
+
+}
